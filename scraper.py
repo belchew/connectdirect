7db0f51bf026fd55a64423f8f1d3ac2c20e3f53a
Konsstantine Belchew <belchev95@icloud.com>
2025-05-22 08:38:17 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,19 +1,17 @@
-import webbrowser
+import requests
 
 # Линкът, който искаш да отвориш
 url = "https://www.seir-sanduk.com/linkzagledane.php?parola=aeagaDs3AdKaAf2"
 
-# Извлича частта след "?" в URL-то
-if "?" in url:
-    url_part = url.split("?")[1]  # Разделя URL-то по "?" и взема частта след него
-else:
-    url_part = ""
+# Изпраща HTTP GET заявка и следи за пренасочване
+response = requests.get(url, allow_redirects=True)
 
-# Отваря линка в браузъра
-webbrowser.open(url)
+# Взима крайния URL (след всички пренасочвания)
+new_url = response.url
 
-# Записва частта от URL в result.txt
+# Записва новия URL в result.txt
 with open("result.txt", "w") as file:
-    file.write(url_part)
+    file.write(new_url)
 
-print("Частта от URL-то е записана в result.txt")
+# Отпечатва новия URL
+print(f"Новият URL е: {new_url}")
