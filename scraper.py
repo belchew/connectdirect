61f5c402a7a4a206fd6f39da29c6fd832675f69e
Konsstantine Belchew <belchev95@icloud.com>
2025-05-22 08:42:54 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import requests
+from urllib.parse import urlparse, parse_qs
 
 # Линкът, който искаш да отвориш
 url = "https://www.seir-sanduk.com/linkzagledane.php?parola=aeagaDs3AdKaAf2"
@@ -6,12 +7,22 @@ url = "https://www.seir-sanduk.com/linkzagledane.php?parola=aeagaDs3AdKaAf2"
 # Изпраща HTTP GET заявка и следи за пренасочване
 response = requests.get(url, allow_redirects=True)
 
-# Взима крайния URL (след всички пренасочвания)
-new_url = response.url
+# Взима крайния URL след всички пренасочвания
+final_url = response.url
 
-# Записва новия URL в result.txt
+# Извлича частта от URL след "pass="
+parsed_url = urlparse(final_url)
+query_params = parse_qs(parsed_url.query)
+
+# Проверява дали параметърът "pass" съществува
+if '?/' in query_params:
+    pass_value = query_params['?/'][0]
+else:
+    pass_value = ""  # Ако параметърът "pass" не съществува, оставяме празно
+
+# Записва стойността след "pass=" в result.txt
 with open("result.txt", "w") as file:
-    file.write(new_url)
+    file.write(pass_value)
 
-# Отпечатва новия URL
-print(f"Новият URL е: {new_url}")
+# Отпечатва стойността след "pass="
+print(f"Паролата е: {pass_value}")
